42b0faae2f1189917e6da88527b90d621f28ff5c
asanda-ngcobo <asanda.ngcobo@outlook.com>
2025-06-23 22:37:16 +0200

    bunner fix

--- app/(website)/_components/MainBunner.js
+++ app/(website)/_components/MainBunner.js
@@ -9,7 +9,7 @@ const MainFont = Nunito_Sans({
 
 function MainBunner({children}) {
     return (
-        <div className={`${MainFont.className} bg-[#04284B] w-[90%] h-[350px] ml-[5%] mt-[200px] absolute rounded-2xl sm:w-[80%]
+        <div className={`${MainFont.className} bg-[#04284B] w-[90%] h-[350px] ml-[5%] mt-[230px] absolute rounded-2xl sm:w-[80%]
          sm:h-[320px] sm:mt-[300px] md:mt-[600px] md:w-[80%] md:h-[300px] md:ml-[5%]
          lg:mt-[290px] lg:w-[40%] lg:h-[240px] xl:mt-[500px] 2xl:mt-[400px] xl:w-[30%] xl:h-[280px]
          grid grid-cols-1 grid-rows-3 items-center justify-center 
