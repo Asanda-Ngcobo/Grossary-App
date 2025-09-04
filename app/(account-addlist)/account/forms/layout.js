1602767296ac94ca06ad07ddca9da5653466bf1b
asanda-ngcobo <asanda.ngcobo@outlook.com>
2025-09-04 14:29:25 +0200

    adding goggle analytics

--- app/(account-addlist)/account/forms/layout.js
+++ app/(account-addlist)/account/forms/layout.js
@@ -1,6 +1,7 @@
 import { Nunito_Sans } from "next/font/google";
 import "@/app/globals.css";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 
 
@@ -17,6 +18,21 @@ export default function FormsLayout({ children }) {
 
   return (
     <html>
+           <head>
+       
+  <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JW5VT5KYJX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JW5VT5KYJX');
+          `}
+        </Script>
+      </head>
        <body
         className={`${MainFont.className} bg-[#041527] text-white h-screen 
         gap-12`}
--- app/(authentication)/layout.js
+++ app/(authentication)/layout.js
@@ -4,6 +4,7 @@ import { Nunito_Sans } from "next/font/google";
 import "../globals.css";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 
 const MainFont = Nunito_Sans({
@@ -17,6 +18,21 @@ export default async function AuthLayout({ children }) {
   
   return (
     <html lang="en">
+           <head>
+             
+        <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-JW5VT5KYJX"
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-JW5VT5KYJX');
+                `}
+              </Script>
+            </head>
       <body
         className={`${MainFont.className}`}
       >
