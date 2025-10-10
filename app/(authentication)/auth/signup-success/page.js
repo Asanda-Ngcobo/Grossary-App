2510ec432a8553bed170a295250905146512c4f8
asanda-ngcobo <asanda.ngcobo@outlook.com>
2025-10-11 00:56:44 +0200

    centering the signup success bunner

--- app/(authentication)/auth/signup-success/page.js
+++ app/(authentication)/auth/signup-success/page.js
@@ -9,8 +9,8 @@ const LogoFont = Quicksand({
 export default function Page() {
   return (
     <main className="grid items-center">
-           <div className="py-6 px-4 rounded-md w-[90%] mt-[5%]
-     ml-[5%] md:w-[40%] md:ml-[30%] text-center bg-white shadow">
+           <div className=" py-6 px-4 rounded-md w-[90%] mt-[40%]
+     ml-[5%] md:w-[40%] md:ml-[30%] md:mt-[5%] text-center bg-white shadow">
       <h1 className={`${LogoFont.className} text-[20px] font-semibold mb-2`}>
        Check your inbox to confirm your email.
       </h1>
