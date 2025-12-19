dcf8393721999330bd7ab0f0d09ff1f5714a01ad
asanda-ngcobo <asanda.ngcobo@outlook.com>
2025-12-19 22:08:42 +0200

    hgjbj

--- app/(account-addlist)/account/forms/[listId]/_listcomponents/AddPriceForm.js
+++ app/(account-addlist)/account/forms/[listId]/_listcomponents/AddPriceForm.js
@@ -59,7 +59,9 @@ export default function AddPricePage({ itemId, itemName, itemBrand, itemNumber,
 
         <button
           type="submit"
-   className={`${ButtonFont.className} bg-[#A2B06D] min-w-[100px] h-[40px] rounded-[5px] text-white font-semibold hover:bg-[#6f7a46]`}
+   className={`${ButtonFont.className} bg-amber-700
+    min-w-[100px] h-[40px] rounded-[5px] text-white font-semibold
+     hover:bg-amber-600`}
           disabled={loading}
         >
           {loading ? 'Saving...' : 'Save Price'}
