9ebb00c7a37b45725ec218ac5d58cd81e994a99a
asanda-ngcobo <asanda.ngcobo@outlook.com>
2026-01-02 20:03:38 +0200

    Synching the loading state

--- app/(account-addlist)/account/forms/[listId]/add-price/[itemId]/loading.js
+++ app/(account-addlist)/account/forms/[listId]/add-price/[itemId]/loading.js
@@ -1,16 +1,126 @@
-
-
-function Loading() {
-  return (
-    <div className="flex flex-col justify-center items-center absolute inset-0 z-50 bg-[#04284B] w-full h-full">
-     
-     
-      <div className="animate-spin rounded-full
-       h-20 w-20 border-t-4 border-[#ACF532]">
-
+function loading() {
+    return (
+        <section className="mt-[10%] lg:mt-[10%] w-[80%]
+        h-[80vh] mx-[10%]">
+               <div className="mx-auto w-full rounded-md  p-6">
+  <div className="flex animate-pulse space-x-4">
+  
+    <div className="flex-1 space-y-6 py-1">
+      <div className="h-2 rounded bg-[#04284B]"></div>
+      <div className="space-y-3">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+              <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+        </div>
+        <div className="h-2 rounded bg-[#04284B]"></div>
       </div>
     </div>
-  )
+  </div>
+</div>
+
+               <div className="mx-auto w-full rounded-md  p-4">
+  <div className="flex animate-pulse space-x-4">
+  
+    <div className="flex-1 space-y-6 py-1">
+      <div className="h-2 rounded bg-[#04284B]"></div>
+      <div className="space-y-3">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+              <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+        </div>
+        <div className="h-2 rounded bg-[#04284B]"></div>
+      </div>
+    </div>
+  </div>
+</div>
+               <div className="mx-auto w-full rounded-md  p-4">
+  <div className="flex animate-pulse space-x-4">
+  
+    <div className="flex-1 space-y-6 py-1">
+      <div className="h-2 rounded bg-[#04284B]"></div>
+      <div className="space-y-3">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+              <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+        </div>
+        <div className="h-2 rounded bg-[#04284B]"></div>
+      </div>
+    </div>
+  </div>
+</div>
+               <div className="mx-auto w-full rounded-md  p-4">
+  <div className="flex animate-pulse space-x-4">
+  
+    <div className="flex-1 space-y-6 py-1">
+      <div className="h-2 rounded bg-[#04284B]"></div>
+      <div className="space-y-3">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+              <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+        </div>
+        <div className="h-2 rounded bg-[#04284B]"></div>
+      </div>
+    </div>
+  </div>
+</div>
+               <div className="mx-auto w-full rounded-md  p-4">
+  <div className="flex animate-pulse space-x-4">
+  
+    <div className="flex-1 space-y-6 py-1">
+      <div className="h-2 rounded bg-[#04284B]"></div>
+      <div className="space-y-3">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+              <div className="col-span-2 h-2 rounded bg-[#04284B]"></div>
+          <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+             <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+                   <div className="col-span-1 h-2 rounded bg-[#04284B]"></div>
+        </div>
+        <div className="h-2 rounded bg-[#04284B]"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</section>
+    )
 }
 
-export default Loading
+export default loading
