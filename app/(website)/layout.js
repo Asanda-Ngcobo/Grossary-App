823077229021802de1cfc9d815fd9e66865870b0
asanda-ngcobo <asanda.ngcobo@outlook.com>
2026-06-17 22:23:16 +0200

    adding home analytics

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -1,7 +1,6 @@
 import { Nunito_Sans } from "next/font/google";
 import "../globals.css";
 import Navigation from "@/app/(website)/_components/Navigation";
-import Footer from "@/app/(website)/_components/Footer";
 import { Analytics } from "@vercel/analytics/next"
 import Script from "next/script";
 import AppNavigation from "../(account)/_ui/AppNav";
@@ -18,8 +17,20 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
-      <head>
-    
+              <head>
+       
+  <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JW5VT5KYJX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JW5VT5KYJX');
+          `}
+        </Script>
       </head>
       <body
         className={`${MainFont.className} gap-6`}
